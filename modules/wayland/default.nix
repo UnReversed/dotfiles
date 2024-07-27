2cf6b6a03af854003fdccd473b62466f88025438
UnReversed <171379891+UnReversed@users.noreply.github.com>
2024-07-27 11:50:31 +0000

    refactor: sddm -> tuigreetd

--- modules/wayland/default.nix
+++ modules/wayland/default.nix
@@ -1,9 +1,20 @@
 {
+  pkgs,
   ...
 }: {
   imports = [
-    ./sddm.nix
     ./fonts.nix
   ];
   programs.hyprland.enable = true;
+  programs.hyprlock.enable = true;
+  services.hypridle.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r --time --asterisks --cmd Hyprland --theme border=blue;container=black;prompt=green;input=red";
+        user = "greeter";
+      };
+    };
+  };
 }
