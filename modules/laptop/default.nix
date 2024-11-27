554a78dbd57ea80808dc84b93d87d3b08e04aa95
UnReversed <171379891+UnReversed@users.noreply.github.com>
2024-11-27 18:25:58 +0100

    feat: add thermald

--- modules/laptop/default.nix
+++ modules/laptop/default.nix
@@ -3,18 +3,21 @@
     ./bluetooth.nix
   ];
 
-  services.auto-cpufreq = {
-    enable = true;
-    settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
+  services = {
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
       };
     };
+    thermald.enable = true;
   };
   powerManagement.powertop.enable = true;
 }
