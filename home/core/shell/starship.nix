bb5b550fdef0e9c401ae6d4664a1df8179cb1d24
UnReversed <171379891+UnReversed@users.noreply.github.com>
2024-11-27 18:26:57 +0100

    feat: starship config

--- home/core/shell/starship.nix
+++ home/core/shell/starship.nix
@@ -2,5 +2,74 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      add_newline = true;
+
+      azure = {
+        disabled = false;
+      };
+
+      bun = {
+        symbol = " ";
+      };
+
+      dart = {
+        symbol = " ";
+      };
+
+      directory = {
+        read_only = " 󰌾";
+      };
+
+      direnv = {
+        disabled = false;
+      };
+
+      docker_context = {
+        symbol = " ";
+      };
+
+      git_branch = {
+        symbol = " ";
+      };
+
+      git_commit = {
+        tag_symbol = "  ";
+      };
+
+      golang = {
+        symbol = " ";
+      };
+
+      hostname = {
+        ssh_symbol = " ";
+      };
+
+      nix_shell = {
+        symbol = " ";
+      };
+
+      nodejs = {
+        symbol = " ";
+        detect_files = [
+          "package.json"
+          ".node-version"
+          "!bunfig.toml"
+          "!bun.lockb"
+        ];
+      };
+
+      package = {
+        symbol = "󰏗 ";
+      };
+
+      python = {
+        symbol = " ";
+      };
+
+      rust = {
+        symbol = "󱘗 ";
+      };
+    };
   };
 }
