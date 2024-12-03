782ca333d95bc1a0a63f4dce550d9cdf03ad8c0d
UnReversed <171379891+UnReversed@users.noreply.github.com>
2024-12-04 00:44:30 +0100

    feat: wlogout

--- home/gui/hyprland/default.nix
+++ home/gui/hyprland/default.nix
@@ -1,9 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hyprland.nix
     ./hypridle.nix
     ./hyprlock.nix
     ./hyprpaper.nix
     ./waybar
+  ];
+  home.packages = with pkgs; [
+    wlogout
   ];
 }
