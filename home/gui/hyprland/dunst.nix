3ed1ae2474b1df2a87a020ffa0c6ff4fc2054090
UnReversed <171379891+UnReversed@users.noreply.github.com>
2024-12-27 14:28:24 +0100

    fix: few changes with services systemd dependency

--- home/gui/hyprland/dunst.nix
+++ home/gui/hyprland/dunst.nix
@@ -1,3 +1,4 @@
-{
+{lib, ...}: {
   services.dunst.enable = true;
+  systemd.user.services.network-manager-applet.Unit.After = lib.mkForce "graphical-session.target";
 }
--- home/gui/tools.nix
+++ home/gui/tools.nix
@@ -19,4 +19,5 @@
     udiskie.enable = true;
   };
   systemd.user.services.blueman-applet.Unit.After = lib.mkForce "graphical-session.target";
+  systemd.user.services.network-manager-applet.Unit.After = lib.mkForce "graphical-session.target";
 }
