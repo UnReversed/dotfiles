0c6b60c1d8c52217e6b681290fd0a6248712a00f
UnReversed <171379891+UnReversed@users.noreply.github.com>
2025-02-10 14:36:43 +0100

    feat: gnome.gnome-keyring

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -15,6 +15,7 @@
   services = {
     udisks2.enable = true;
     fwupd.enable = true;
+    gnome.gnome-keyring.enable = true;
   };
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
