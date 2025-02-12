37394ff456caff60e223a6d3e7aae334f6dcdb12
UnReversed <171379891+UnReversed@users.noreply.github.com>
2025-02-12 09:52:23 +0100

    feat: lanzaboote cache

--- modules/base/nix/default.nix
+++ modules/base/nix/default.nix
@@ -20,6 +20,7 @@
         "https://nixpkgs.cachix.org"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://lanzaboote.cachix.org"
       ];
 
       trusted-public-keys = [
@@ -27,6 +28,7 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk="
       ];
     };
   };
