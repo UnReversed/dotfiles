8b486c87ef93c2b99eb04baf6c7e18b9d26a7ba8
UnReversed <171379891+UnReversed@users.noreply.github.com>
2025-07-25 17:32:46 +0200

    chore(vscode): config + sonarlint

--- home/gui/editors/vscode.nix
+++ home/gui/editors/vscode.nix
@@ -26,21 +26,26 @@
       ms-vscode-remote.remote-ssh
       ms-vscode-remote.remote-ssh-edit
       ms-vscode-remote.remote-containers
+      sonarsource.sonarlint-vscode
     ]);
   shared-userSettings = {
     "editor.fontFamily" = "'Hack Nerd Font','Droid Sans Mono', 'monospace'";
     "terminal.integrated.fontFamily" = "'Hack Nerd Font'";
+    "chat.agent.enabled" = false;
     "terminal.integrated.enableImages" = true;
     "workbench.colorTheme" = "Default Dark+";
     "workbench.iconTheme" = "material-icon-theme";
     "gitblame.inlineMessageEnabled" = true;
     "remote.SSH.localServerDownload" = "off";
+    "remote.SSH.enableX11Forwarding" = false;
+    "remote.SSH.connectTimeout" = 30;
     "remote.SSH.defaultExtensions" = [
       "PKief.material-icon-theme"
       "PKief.material-product-icons"
       "mhutchie.git-graph"
       "waderyan.gitblame"
     ];
+    "sonarlint.pathToNodeExecutable" = "${pkgs.nodejs_24}";
   };
 in {
   programs.vscode = {
@@ -66,6 +71,7 @@ in {
           (with extension; [
             ms-azuretools.vscode-azureresourcegroups
             ms-mssql.mssql
+            ms-azuretools.vscode-azurefunctions
           ])
           ++ (with pkgs.vscode-extensions; [
             ms-dotnettools.csdevkit
@@ -93,4 +99,7 @@ in {
       };
     };
   };
+  home.packages = with pkgs; [
+    temurin-bin-21
+  ];
 }
