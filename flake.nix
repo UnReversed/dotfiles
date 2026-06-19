e062ec8319cb3d013541ced283a43bfaccc8ab64
UnReversed <171379891+UnReversed@users.noreply.github.com>
2026-06-19 17:49:03 +0200

    chore(flake): pin lanzaboote bootspec error
    issue: https://github.com/nix-community/lanzaboote/issues/624

--- flake.nix
+++ flake.nix
@@ -16,7 +16,8 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:nix-community/lanzaboote/0403b4b7e8b2612657f0053a4c315e6c43eee9e6";
+      # url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
